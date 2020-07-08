01d77b73230e06a6fc9ace42bbdcdfed7b1e3371
Tilak Maddy <tilak.madichetti@gmail.com>
2020-07-08 18:35:45 +0530

    Updates

---
+++ precache-manifest.2d2551a919db5f529ebff944f1336836.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "415932543542e725234e9d79d97ed8c0",
+    "url": "/super-giggle%20/index.html"
+  },
+  {
+    "revision": "54f4e807a56c22df6a9f",
+    "url": "/super-giggle%20/static/css/main.50720ed8.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/super-giggle%20/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/super-giggle%20/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54f4e807a56c22df6a9f",
+    "url": "/super-giggle%20/static/js/main.b82a9483.chunk.js"
+  },
+  {
+    "revision": "89dfbb0f646bfa8b8469",
+    "url": "/super-giggle%20/static/js/runtime-main.acf0678f.js"
+  }
+]);

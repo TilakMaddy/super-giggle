1320537814c938d256ffc1c17a2e8a0cad4441fa
Tilak Maddy <tilak.madichetti@gmail.com>
2020-07-08 17:25:36 +0530

    done with barebones 👍

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import  { CardList }  from './components/card-list/card-list.component.jsx';
+import { SearchField } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +10,8 @@ class App extends Component {
 
     //before using 'this', you have to call super()
     this.state = {
-      monsters : []
+      monsters : [],
+      searchField: ''
     }
   }
 
@@ -20,9 +22,12 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className="App">
-        <CardList monsters={ this.state.monsters } />
+        <SearchField placeholder="Search monsters" handleChange={ e => this.setState({ searchField: e.target.value}) } />
+        <CardList monsters={ filteredMonsters } />
       </div>
     );
   }
